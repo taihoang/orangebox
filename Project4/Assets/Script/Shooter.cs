abbd6fcd691090c4e139c538eab43a9a7e5e37e2
taihoang <taihoang@uga.edu>
2016-04-26 19:01:09 -0400

    shooting & sound

---
+++ Project4/Assets/Script/Shooter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+    public AudioClip throwSound;
+    public Rigidbody bullet;
+    public float throwSpeed = 30.0f;
+    public static bool canThrow = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetButtonDown("Fire1") && canThrow)
+        {
+            GetComponent<AudioSource>().PlayOneShot(throwSound);
+            Rigidbody newBullet = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody;
+            newBullet.name = "bullet";
+            newBullet.velocity = transform.forward * throwSpeed;
+            Physics.IgnoreCollision(transform.root.GetComponent<Collider>(), newBullet.GetComponent<Collider>(), true);
+        }
+	}
+
+    
+}
---
+++ Project4/Assets/Script/Swim.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Swim : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.transform.tag == "Player")
+        {
+            var controller = col.gameObject;
+            controller.SendMessage("changeGravity", true);
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if(col.transform.tag == "Player")
+        {
+            var controller = col.gameObject;
+            controller.SendMessage("changeGravity", false);
+        }
+    }
+}
