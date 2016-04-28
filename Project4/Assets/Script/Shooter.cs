1177c0e5fa0e18d75fc53573658fd2d6d4689d65
taihoang <taihoang@uga.edu>
2016-04-27 20:25:06 -0400

    added meteor and fireball attack

--- Project4/Assets/Script/Shooter.cs
+++ Project4/Assets/Script/Shooter.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class Shooter : MonoBehaviour {
     public AudioClip throwSound;
+    public AudioClip throwSound3;
     public Rigidbody bullet;
+    public Rigidbody bullet3;
     public float throwSpeed = 30.0f;
+    private int weapon = 0;
     public static bool canThrow = true;
 	// Use this for initialization
 	void Start () {
@@ -13,13 +16,40 @@ public class Shooter : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            weapon++;
+            if(weapon == 3)
+            {
+                weapon = 0;
+            }
+        }
+
 	    if(Input.GetButtonDown("Fire1") && canThrow)
         {
-            GetComponent<AudioSource>().PlayOneShot(throwSound);
-            Rigidbody newBullet = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody;
-            newBullet.name = "bullet";
-            newBullet.velocity = transform.forward * throwSpeed;
-            Physics.IgnoreCollision(transform.root.GetComponent<Collider>(), newBullet.GetComponent<Collider>(), true);
+            if(weapon == 0)
+            {
+                GetComponent<AudioSource>().PlayOneShot(throwSound);
+                Rigidbody newBullet = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody;
+                newBullet.name = "bullet";
+                newBullet.velocity = transform.forward * throwSpeed;
+               // Physics.IgnoreCollision(transform.root.GetComponent<Collider>(), newBullet.GetComponent<Collider>(), true);
+            } else if(weapon == 1)
+            {
+                GetComponent<AudioSource>().PlayOneShot(throwSound);
+                Rigidbody newBullet = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody;
+                newBullet.name = "bullet";
+                newBullet.velocity = transform.forward * throwSpeed;
+                //Physics.IgnoreCollision(transform.root.GetComponent<Collider>(), newBullet.GetComponent<Collider>(), true);
+            } else if(weapon == 2)
+            {
+                GetComponent<AudioSource>().PlayOneShot(throwSound3);
+                Rigidbody newBullet = Instantiate(bullet3, transform.position, transform.rotation) as Rigidbody;
+                newBullet.name = "bullet3";
+               // newBullet.velocity = transform.forward * throwSpeed;
+                //Physics.IgnoreCollision(transform.root.GetComponent<Collider>(), newBullet.GetComponent<Collider>(), true);
+            }
+           
         }
 	}
 
